17450cf51219894311c287b4555b6af83506e34d
arf1e <anotheruselessbox@gmail.com>
2023-10-26 01:26:42 +0300

    introduce pnpm, update github action, books hotfix

--- middlewares/error.ts
+++ middlewares/error.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 
 export function errorLoggingMiddleware(
-  _error: Error,
+  error: Error,
   _req: Request,
   res: Response,
   _next: NextFunction
 ) {
   console.log("Houston, we've got a problem:");
-  console.log(_error);
-  res.json({ msg: _error.message });
+  console.log(error);
+  res.json({ msg: error.message });
 }

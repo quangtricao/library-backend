fb2e0377d1a19b9b3da2ee005ad227aaa8c5e132
JuliaThTranNguyen <trhien541@gmail.com>
2023-11-06 20:25:04 +0200

    refactored: id validation

--- middlewares/idValidator.ts
+++ middlewares/idValidator.ts
@@ -2,15 +2,15 @@ import { z } from 'zod';
 import { CollectionType } from '../types/db';
 import { NextFunction, Request, Response } from 'express';
 
-const idSchema = z.string().regex(/^[a-fA-F\d]{24}$/, 'Invalid ID');
-
 export const validateId = <K extends keyof CollectionType>(_key?: K) => {
+  const idSchema = z.string().regex(/^[a-fA-F\d]{24}$/, 'Invalid ID');
+  
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await idSchema.parseAsync(req.params.id);
       next();
     } catch (error) {
-      res.status(400).json(error instanceof z.ZodError ? error.errors : error);
+      res.status(400).json(error);
     }
   };
 };

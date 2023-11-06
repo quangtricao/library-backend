3a56d3d84dc07e9a9491b261bb5cbcc2c92c9677
JuliaThTranNguyen <trhien541@gmail.com>
2023-11-06 18:44:09 +0200

    v6 new id validation - Zod regex()

--- middlewares/idValidator.ts
+++ middlewares/idValidator.ts
@@ -2,22 +2,15 @@ import { z } from 'zod';
 import { CollectionType } from '../types/db';
 import { NextFunction, Request, Response } from 'express';
 
-export const validateId = <K extends keyof CollectionType>(_key: K) => {
-  const idSchema = z.string().refine(value => {
-    // Validate that the ID is a 24-character hexadecimal string.
-    const pattern = /^[a-fA-F\d]{24}$/;
-    return pattern.test(value);
-  }, {
-    message: 'Invalid ID format for the specified collection.',
-  });
+const idSchema = z.string().regex(/^[a-fA-F\d]{24}$/, 'Invalid ID');
 
+export const validateId = <K extends keyof CollectionType>(_key?: K) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await idSchema.parseAsync(req.params.id);
       next();
     } catch (error) {
-      res.status(400).json({error});
+      res.status(400).json(error instanceof z.ZodError ? error.errors : error);
     }
   };
 };
-

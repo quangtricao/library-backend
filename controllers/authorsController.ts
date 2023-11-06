277650aa434a076f7e9f04d98e10708cbabdab98
elis1386 <elis1386@gmail.com>
2023-11-06 11:47:26 +0200

    delete extra s

--- controllers/authorsController.ts
+++ controllers/authorsController.ts
@@ -1,27 +1,27 @@
 import { Request, Response } from 'express';
-import AuthorsServices from '../services/authorsService';
+import AuthorsService from '../services/authorsService';
 
 export const getAllAuthors = async (_req: Request, res: Response) => {
-  const authors = await AuthorsServices.getAll();
+  const authors = await AuthorsService.getAll();
   res.json(authors);
 };
 
 export const createAuthor = async (req: Request, res: Response) => {
-  const createdAuthor = await AuthorsServices.createNewAuthor(req.body);
+  const createdAuthor = await AuthorsService.createNewAuthor(req.body);
   res.status(201).json(createdAuthor);
 };
 
 export const getAuthorById = async (req: Request, res: Response) => {
-  const author = await AuthorsServices.getById(req.params.id);
+  const author = await AuthorsService.getById(req.params.id);
   res.json(author);
 };
 
 export const updateAuthor = async (req: Request, res: Response) => {
-  const updatedAuthor = await AuthorsServices.updateById(req.params.id, req.body);
+  const updatedAuthor = await AuthorsService.updateById(req.params.id, req.body);
   res.json(updatedAuthor);
 };
 
 export const deleteAuthor = async (req: Request, res: Response) => {
-  await AuthorsServices.deleteById(req.params.id);
+  await AuthorsService.deleteById(req.params.id);
   res.status(204).send();
 };

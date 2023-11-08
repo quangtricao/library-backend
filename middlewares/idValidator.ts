9a0cf0d1ab8bf0137f1d4dd9d5971e84df890a99
JuliaThTranNguyen <trhien541@gmail.com>
2023-11-08 22:06:03 +0200

    sync with the main branch

--- middlewares/idValidator.ts
+++ middlewares/idValidator.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
 import { NextFunction, Request, Response } from 'express';
 
+export const idSchema = z.string().regex(/^[a-fA-F\d]{24}$/, 'Invalid ID');
+
 export const validateId = async (req: Request, res: Response, next: NextFunction) => {
-  const idSchema = z.string().regex(/^[a-fA-F\d]{24}$/, 'Invalid ID');
   try {
     await idSchema.parseAsync(req.params.id);
     next();

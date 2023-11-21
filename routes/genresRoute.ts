3fb35f1ff77523906c546effe7f3440c058c4b38
Quang Tri Cao <caoquangtri0110@gmail.com>
2023-11-21 14:06:09 +0200

    Genres: Add auth protection middlewares

--- routes/genresRoute.ts
+++ routes/genresRoute.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import genreController from '../controllers/genresController';
-import { validateGenreDtoInput } from '../middlewares/genresValidate';
 import { passThrowsToMiddleware } from '../utils/passThrowsToMiddleware';
 import { validateId } from '../middlewares/idValidator';
+import { validateGenreDtoInput } from '../middlewares/genresValidate';
+import { checkAuth } from '../middlewares/checkAuth';
+import { checkAdminRoleOrOwnership } from '../middlewares/checkAdminRoleOrOwnership';
 
 export const genresRouter = express.Router();
 
@@ -11,17 +13,18 @@ genresRouter.use('/:id', validateId);
 genresRouter.get('/', passThrowsToMiddleware(genreController.getAllGenre));
 genresRouter.get('/:id', passThrowsToMiddleware(genreController.getOneGenre));
 genresRouter.get('/:id/books', passThrowsToMiddleware(genreController.getAllBooksByGenre));
-
 genresRouter.post(
   '/',
+  checkAuth,
+  checkAdminRoleOrOwnership,
   validateGenreDtoInput,
   passThrowsToMiddleware(genreController.createGenre)
 );
-
 genresRouter.put(
   '/:id',
+  checkAuth,
+  checkAdminRoleOrOwnership,
   validateGenreDtoInput,
   passThrowsToMiddleware(genreController.updateGenre)
 );
-
 genresRouter.delete('/:id', passThrowsToMiddleware(genreController.deleteGenre));

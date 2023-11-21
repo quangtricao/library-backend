eb927f2beb7dce25ca242cb337d4e14b210e02f2
Quang Tri Cao <caoquangtri0110@gmail.com>
2023-11-21 15:28:32 +0200

    Genres: Change params schema

--- routes/genresRoute.ts
+++ routes/genresRoute.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import genreController from '../controllers/genresController';
+import genresValidator from '../middlewares/genresValidator';
 import { passThrowsToMiddleware } from '../utils/passThrowsToMiddleware';
 import { validateId } from '../middlewares/idValidator';
-import { validateGenreDtoInput } from '../middlewares/genresValidate';
 import { checkAuth } from '../middlewares/checkAuth';
 import { checkAdminRoleOrOwnership } from '../middlewares/checkAdminRoleOrOwnership';
 
@@ -10,21 +10,25 @@ export const genresRouter = express.Router();
 
 genresRouter.use('/:id', validateId);
 
-genresRouter.get('/', passThrowsToMiddleware(genreController.getAllGenre));
+genresRouter.get(
+  '/',
+  genresValidator.validateGenreParamQuery,
+  passThrowsToMiddleware(genreController.getAllGenre)
+);
 genresRouter.get('/:id', passThrowsToMiddleware(genreController.getOneGenre));
 genresRouter.get('/:id/books', passThrowsToMiddleware(genreController.getAllBooksByGenre));
 genresRouter.post(
   '/',
   checkAuth,
   checkAdminRoleOrOwnership,
-  validateGenreDtoInput,
+  genresValidator.validateGenreDtoInput,
   passThrowsToMiddleware(genreController.createGenre)
 );
 genresRouter.put(
   '/:id',
   checkAuth,
   checkAdminRoleOrOwnership,
-  validateGenreDtoInput,
+  genresValidator.validateGenreDtoInput,
   passThrowsToMiddleware(genreController.updateGenre)
 );
 genresRouter.delete('/:id', passThrowsToMiddleware(genreController.deleteGenre));

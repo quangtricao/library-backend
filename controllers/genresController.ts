a7f0120d5712aaf1747969db9dec9288e7f12ff7
Quang Tri Cao <caoquangtri0110@gmail.com>
2023-11-23 16:09:49 +0200

    genresController: Use respondWith

--- controllers/genresController.ts
+++ controllers/genresController.ts
@@ -4,46 +4,44 @@ import genreService from '../services/genresService';
 import { GenreDTO } from '../types/genres';
 import { getPaginationOptionsFromQuery } from '../utils/pagination';
 import { getGetAllGenresOptionsFromQuery } from '../utils/genres';
+import respondWith from '../utils/respondWith';
 
 const getAllGenre = async (req: Request, res: Response) => {
   const options = await getGetAllGenresOptionsFromQuery(req.query);
   const genreInDB = await genreService.getAll(options);
-  res.status(200).json(genreInDB);
+  respondWith(res, { code: 200, data: genreInDB });
 };
 
 const getOneGenre = async (req: Request, res: Response) => {
   const id = req.params.id;
   const foundGenre = await genreService.getOne(id);
-  res.status(200).json(foundGenre);
+  respondWith(res, { code: 200, data: foundGenre });
 };
 
 const getAllBooksByGenre = async (req: Request, res: Response) => {
   const id = req.params.id;
   const pagination = await getPaginationOptionsFromQuery(req.query);
   const allBooks = await genreService.getAllBooks(id, pagination);
-  res.status(200).json(allBooks);
+  respondWith(res, { code: 200, data: allBooks });
 };
 
 const createGenre = async (req: Request, res: Response) => {
   const body = req.body as GenreDTO;
-
   const newGenre = await genreService.create(body);
-  res.status(201).json(newGenre);
+  respondWith(res, { code: 201, data: newGenre });
 };
 
 const updateGenre = async (req: Request, res: Response) => {
   const id = req.params.id;
   const body = req.body as GenreDTO;
-
   const updatedGenre = await genreService.update(id, body);
-  res.status(200).json(updatedGenre);
+  respondWith(res, { code: 200, data: updatedGenre });
 };
 
 const deleteGenre = async (req: Request, res: Response) => {
   const id = req.params.id;
-
   await genreService.remove(id);
-  res.status(204).end();
+  respondWith(res, { code: 204 });
 };
 
 export default {

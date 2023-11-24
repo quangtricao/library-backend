d4251db1462952880771bc2c3c632d5b27ebf004
Quang Tri Cao <caoquangtri0110@gmail.com>
2023-11-24 10:02:58 +0200

    Genre test: Update test cases for genresValidator

--- middlewares/genresValidator.ts
+++ middlewares/genresValidator.ts
@@ -1,4 +1,3 @@
-import { z } from 'zod';
 import { NextFunction, Request, Response } from 'express';
 import { genreSchema } from '../schemas/genre';
 import { getGetAllGenresOptionsFromQuery } from '../utils/genres';
@@ -8,14 +7,7 @@ async function validateGenreDtoInput(req: Request, res: Response, next: NextFunc
     await genreSchema.parseAsync(req.body);
     return next();
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      const newError = error.issues.map((err) => ({
-        path: err.path,
-        error: err.message,
-      }));
-      return res.status(400).json(newError);
-    }
-    next();
+    res.status(400).json(error);
   }
 }
 

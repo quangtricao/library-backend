96794ed84b3dd3df62ddf28be92f0f066be81b07
Quang Tri Cao <caoquangtri0110@gmail.com>
2023-12-10 00:41:15 +0200

    add cors

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import 'dotenv-expand/config';
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes';
 import { routeNotFound } from './middlewares/routeNotFound';
 import { loggingMiddleware } from './middlewares/logging';
@@ -8,11 +9,17 @@ import { errorLoggingMiddleware } from './middlewares/error';
 import { connectMongoDB } from './config/mongoose';
 
 const app = express();
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 connectMongoDB();
 
 // Body parser middleware for application/json
 app.use(express.json());
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(loggingMiddleware);
 // So we can also do /api/v1/ or /api/v2/ etc.
